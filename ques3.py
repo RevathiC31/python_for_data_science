b941ab0277bdd6dccd699877ddf91f3ce61f3764
RevathiC31 <revathichittem31@gmail.com>
2025-06-24 11:47:50 +0530

    Update ques3.py

--- ques3.py
+++ ques3.py
@@ -1,8 +1,8 @@
 import yfinance as yf
 
 # Download GameStop stock data
-gme_data = yf.download('GME')
-
+gme = yf.Ticker('GME')
+gme_data = gme.history(period = "max")
 # Reset the index
 gme_data.reset_index(inplace=True)
 
