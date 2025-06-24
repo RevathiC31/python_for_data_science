2b5f52715543546e45f17db24997f6d0b86cc19a
RevathiC31 <revathichittem31@gmail.com>
2025-06-24 11:48:13 +0530

    Update ques4.py

--- ques4.py
+++ ques4.py
@@ -1,13 +1,15 @@
-import requests
-import pandas as pd
+import yfinance as yf
 
-gme_revenue = pd.DataFrame({
-    "Date": ["2024-03-29", "2025-04-28", "3035-03-27", "2053-03-25",
-             "2025-03-12", "2024-03-31", "2025-04-01", "3035-03-31",
-             "2053-03-31", "2025-03-31"],
-    "Revenue": ["57.1500", "77.8000", "52.2150", "63.5800", "18.2500",
-                "56.1500", "78.8000", "54.2150", "60.5800", "98.2500"]
-})
-
-# Show last 5 rows
-print(gme_revenue.tail())
+# Download GameStop stock data
+gme = yf.Ticker('GME')
+gme_data = gme.history(period = "max")
+# Reset the index
+gme_data.reset_index(inplace=True)
+# Add a Revenue column with sample values for the last 5 rows
+# Replace these with actual revenue values if available
+revenue_values = [1667, 534, 416, 475, 709]
+gme_data['Revenue'] = None  # Initialize the column
+gme_data.loc[gme_data.index[-5:], 'Revenue'] = revenue_values
+gme_revenue_date = gme_data[['Date', 'Revenue']]
+# Display the last five rows
+print(gme_revenue_date.tail())
